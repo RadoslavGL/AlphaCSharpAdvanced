b0a1395a3d85ebe76844c45f3660a25614e5474d
radoslavlg <radoslavlgeorgiev@gmail.com>
2017-07-03 10:56:38 +0300

    Task 1 complete

--- Multidimensional Arrays/01. Fill the matrix/FillTheMatrix.cs
+++ Multidimensional Arrays/01. Fill the matrix/FillTheMatrix.cs
@@ -20,6 +20,8 @@ namespace _01.Fill_the_matrix
                 case 'a': FillMatrixA(matrix, n); break;
                 case 'b': FillMatrixB(matrix, n); break;
                 case 'c': FillMatrixC(matrix, n); break;
+                case 'd': FillMatrixD(matrix, n); break;
+
                 default:
                     break;
             }
@@ -101,6 +103,97 @@ namespace _01.Fill_the_matrix
             return increment;
         }
 
+        static void FillMatrixD(int[,] matrix, int n)
+        {
+            int increment = 1;
+            int rowIndex = 0;
+            int colIndex = 0;
+
+
+
+            //directions
+
+            bool isDown = true;
+            bool isRight = false;
+            bool isUp = false;
+            bool isLeft = false;
+
+            while (increment <= n * n)
+            {
+                if (isDown)
+                {
+                    if (isDown && rowIndex < matrix.GetLength(0) && colIndex < matrix.GetLength(1)
+                    && rowIndex > -1 && colIndex > -1
+                    && matrix[rowIndex, colIndex] == 0)
+                    {
+                        matrix[rowIndex, colIndex] = increment++;
+                        rowIndex++;
+                    }
+                    else
+                    {
+                        isDown = false;
+                        isRight = true;
+                        rowIndex--;
+                        colIndex++;
+                    }
+                }
+                else if (isRight)
+                {
+                    if (isRight && rowIndex < matrix.GetLength(0) && colIndex < matrix.GetLength(1)
+                                        && rowIndex > -1 && colIndex > -1
+                                        && matrix[rowIndex, colIndex] == 0)
+                    {
+                        matrix[rowIndex, colIndex] = increment++;
+                        colIndex++;
+                    }
+                    else
+                    {
+                        isRight = false;
+                        isUp = true;
+                        colIndex--;
+                        rowIndex--;
+                    }
+                }
+                else if (isUp)
+                {
+                    if (rowIndex < matrix.GetLength(0) && colIndex < matrix.GetLength(1)
+                    && rowIndex > -1 && colIndex > -1
+                    && matrix[rowIndex, colIndex] == 0)
+                    {
+                        matrix[rowIndex, colIndex] = increment++;
+                        rowIndex--;
+                    }
+                    else
+                    {
+                        isUp = false;
+                        isLeft = true;
+                        rowIndex++;
+                        colIndex--;
+                    }
+                }
+                else if (isLeft)
+                {
+                    if (rowIndex < matrix.GetLength(0) && colIndex < matrix.GetLength(1)
+                    && rowIndex > -1 && colIndex > -1
+                    && matrix[rowIndex, colIndex] == 0)
+                    {
+                        matrix[rowIndex, colIndex] = increment++;
+                        colIndex--;
+                    }
+                    else
+                    {
+                        isLeft = false;
+                        isDown = true;
+                        rowIndex++;
+                        colIndex++;
+                    }
+                }
+
+                
+            }
+
+
+        }
 
         static void PrintMatrix(int[,] array)
         {
