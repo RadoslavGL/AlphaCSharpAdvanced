18b5c5dab5c6819627e4663558b309b9f798dfdc
RadoslavLG <radoslavlgeorgiev@gmail.com>
2017-07-05 18:25:08 +0300

    sdsadas

---
+++ Strings/06.StringLength/StringLength.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.StringLength
+{
+    class StringLength
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(input);
+
+            if (input.Length < 20)
+            {
+                Console.WriteLine("{0}", input.PadRight(20, '*'));
+            }
+            else
+            {
+                Console.WriteLine(input);
+            }
+
+        }
+    }
+}
---
+++ Strings/08.ExtractSentences/ExtractSentences.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.ExtractSentences
+{
+    class ExtractSentences
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = "";
+
+            int startIndex = 0;
+            int endIndex = 0;
+
+        }
+    }
+}
